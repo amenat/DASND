b6c22954b9dd05a26782dbd020546b0fcd6311d7
Ankush Menat <ankushmenat@gmail.com>
2020-03-28 18:52:59 +0530

    finish task2

--- Project0/Task2.py
+++ Project0/Task2.py
@@ -16,7 +16,23 @@ TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
 Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
 September 2016.".
 """
 
+# column definition
+incoming, answering, ts, duration = range(4)
+
+callsT = list(zip(*calls))
+nums = set(callsT[incoming] + callsT[answering])
+
+# initialize dictionary with 0 duration for all phone numbers
+time_spent = {num:0 for num in nums}
+
+for call in calls:
+    time_spent[call[incoming]] += int(call[duration])
+    time_spent[call[answering]] += int(call[duration])
+
+max_caller = max(time_spent, key=lambda x: time_spent[x])
+
+print(f'{max_caller} spent the longest time, {time_spent[max_caller]} seconds, on the phone during September 2016.')
