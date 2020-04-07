adc5fa0bf72d3b91d5a34d8d6c69abd8acb4a9c3
Ankush Menat <ankushmenat@gmail.com>
2020-04-07 23:27:11 +0530

    add note about AD runtime

--- Project1 data structures/Problem4 AD/ad_lookup.py
+++ Project1 data structures/Problem4 AD/ad_lookup.py
@@ -35,6 +35,7 @@ def is_user_in_group(user: str, group: Group) -> bool:
     This implies that we will have to check each and every group/subgroup recursively and if user can be part of any and all groups.
 
     *If users are unique a particular group* then this reduces to near linear time: O(len(groups) + len(all_users))
+    Since Class Group doesn't ensure uniqueness of users in anyway, I've assumed that users can be duplicated across groups.
 
     '''
     # search in users of current group
