1d9ba4304b71ff6f973f05c66a767f44361ecaa9
Ankush Menat <ankushmenat@gmail.com>
2020-04-10 18:24:19 +0530

    Working text compression

--- Project1 data structures/Problem3 huffman/test_huffman_alice.py
+++ Project1 data structures/Problem3 huffman/test_huffman_alice.py
@@ -1,8 +1,8 @@
 from huffman import huffman_encoding, huffman_decoding
-
+import pickle
 
 # ref: https://stackoverflow.com/questions/16887493/write-a-binary-integer-or-string-to-a-file-in-python
-def bit_string_to_file(bitstring, filename):
+def bit_string_to_file(bitstring: str, filename: str) -> None:
     bit_strings = [bitstring[i:i + 8] for i in range(0, len(bitstring), 8)]
     byte_list = [int(b, 2) for b in bit_strings]
 
@@ -10,21 +10,40 @@ def bit_string_to_file(bitstring, filename):
         f.write(bytearray(byte_list))  # convert to bytearray before writing
 
 
-def file_to_bitstring(file):
-    pass
-
+# debug this
+def file_to_bitstring(filename: str) -> str:
+    with open(filename, 'rb') as f:
+        content = f.read()
+    bitstring = ''
+    for byte in content:
+        bitstring += '{0:08b}'.format(byte)
+    return bitstring
 
 
 if __name__ == "__main__":
-    with open('alice.txt') as alice:
+    # TODO: Fix trailing newline issue
+    with open('alice.txt', encoding='utf-8') as alice:
         data = alice.read()
     bitstring, tree = huffman_encoding(data)
-    compressed_file = 'alice.dat'
 
+    compressed_file = 'alice.dat'
     bit_string_to_file(bitstring, compressed_file)
-    to_decode = file_to_bitstrineg(compressed_file)
+
+    # Store tree in file
+    with open('alice.tree', 'wb') as f:
+        pickle.dump(tree, f)
+
+
+    # create bitstring
+    to_decode = file_to_bitstring(compressed_file)
+
+    # retrieve tree in file
+    with open('alice.tree', 'r+b') as f:
+        tree = pickle.load(f)
 
     decoded = huffman_decoding(to_decode, tree)
 
+    print(len(bitstring))
+    print(len(to_decode))
     print(f'Does decoded data match orginal? : {data == decoded}')
 
