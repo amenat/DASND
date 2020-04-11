51acef17574941dde191705f9848b74e3aad3b51
Ankush Menat <ankushmenat@gmail.com>
2020-04-11 23:19:55 +0530

    Finalize blockchain

--- Project1 data structures/Problem5 Blockchain/blockchain.py
+++ Project1 data structures/Problem5 Blockchain/blockchain.py
@@ -1,55 +1,80 @@
 import hashlib
 import time
+from typing import Union
 
 
 
 class Block:
 
-    def __init__(self, timestamp: str,  data: str, previous_hash: str = '0') -> None:
-      self.timestamp = timestamp
-      self.data = data
-      self.previous_hash = previous_hash
-      self.hash = self._calc_hash()
-      self.prevous_block = None
+    def __init__(self, timestamp: str,  data: str, previous_hash: Union[str, None]=None, idx=0):
+
+        if not isinstance(timestamp, str): raise ValueError('timestamp should be of type str')
+        if not isinstance(data, str): raise ValueError('data should be of type str')
+        if not (isinstance(previous_hash, str) or previous_hash is None): raise ValueError('previous_hash should be str or None')
+        self.idx = idx
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.prevous_block = None
+        self.hash = self._calc_hash()
 
     def _calc_hash(self) -> str:
         sha = hashlib.sha256()
-        sha.update((self.data + self.timestamp + self.previous_hash).encode())
+        sha.update((self.data + self.timestamp + str(self.previous_hash)).encode())
         return '0x' + sha.hexdigest()
 
-    def __repr__(self):
-        return f"Block: (data: {self.data[:5]}, hash: {self.hash[:7]})"
+    def __repr__(self) -> str:
+        return f"Block {self.idx}: (ts: {self.timestamp}, hash: {self.hash[:10]}, data: \'{self.data[:10]}\')"
 
 
 class BlockChain:
     def __init__(self):
         self.tail = None
+        self.size = 0
 
     def append(self, data: str) -> None:
+        if not isinstance(data, str): raise ValueError('Data should be of type `str`')
+
         ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
         if self.tail is None:
-            new_block = Block(ts, data, '0')
-            self.tail = new_block
+            new_block = Block(ts, data, None, self.size)
         else:
-            new_block = Block(ts, data, self.tail.hash)
+            new_block = Block(ts, data, self.tail.hash, self.size)
             new_block.prevous_block = self.tail
-            self.tail  = new_block
 
-    def __str__(self):
-        string = ""
+        self.tail  = new_block
+        self.size += 1
+
+    def __repr__(self):
+        string = "--- tail of blockchain ---\n"
         tail = self.tail
         while tail:
-            string = str(tail) + ' <- ' + string
+            string += str(tail) + '\n'
             tail = tail.prevous_block
+
+        string += "--- beginning of blockchain ---\n"
         return string
 
 
 if __name__ == "__main__":
     chain = BlockChain()
     chain.append('Hello, ')
+    time.sleep(1) # to simulate different timestamps
     chain.append('World!')
+    time.sleep(1) # to simulate different timestamps
 
-    # to check if two same datablocks generate different hashes.
+    # to check if two same data generate different hashes.
     chain.append('Foo')
+    time.sleep(1) # to simulate different timestamps
     chain.append('Foo')
+    time.sleep(1) # to simulate different timestamps
     print(chain)
+
+    print('Append empty string block')
+    chain.append('') # appending empty string shouldn't be a problem
+    print(chain)
+
+    # blocks can't be manually appended
+    new_block = Block('FakeTS', 'Hello world', None, 1)
+    chain.append(new_block)   # should raise ValueError
+
--- Project1 data structures/explanation_5.md
+++ Project1 data structures/explanation_5.md
@@ -1,2 +1,12 @@
 Problem 5: block chain
 
+Block chain is implemented by creating a linked list that has pointer to tail instead of head. In a way it's a stack without pop operation.
+
+Block class stores data in string, calculates and udpates unique hash. Uniqueness of hash is ensured by concatinating data with prevous hash and timestamp.
+
+
+Runtime complexity: O(1) to insert data block into blockchain. There are only constant time operations happening in append.
+
+I couldn't find any info on complexity of sha256 hash calculation. In worst case scenario I think it would be O(n) where n is length of string.
+
+Space complexity: O(n) grows with respect to data blocks and amount of data stored in datablocks. No additional space is required.
