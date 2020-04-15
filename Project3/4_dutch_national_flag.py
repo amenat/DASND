64cf4e0416230a71eb9b778029fd3419ff2412d9
Ankush Menat <ankushmenat@gmail.com>
2020-04-15 19:24:59 +0530

    Solve dutch national

--- Project3/4_dutch_national_flag.py
+++ Project3/4_dutch_national_flag.py
@@ -1,23 +1,40 @@
-'''
-Dutch National Flag Problem
+from typing import List, Union
 
-Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal. You're not allowed to use any sorting function that Python provides.
 
-Note: O(n) does not necessarily mean single-traversal. For e.g. if you traverse the array twice, that would still be an O(n) solution but it will not count as single traversal.
-
-Here is some boilerplate code and test cases to start with:
-'''
-
-from typing import List
-
-def sort_012(input_list: List[int]) -> List[int]:
+def sort_012(input_list: List[int]) -> Union[List[int], None]:
     """
-    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+    Given an input array consisting on only 0, 1, and 2, sort the array in a
+    single traversal. Returns None if input list contains any other value than
+    0, 1 or 2.
 
     Args:
        input_list(list): List to be sorted
     """
-    pass
+
+    # copying list is essential otherwise we will end up modifying the original
+    # list object. If we count these as "treversal" then it's impossible to
+    # solve the given problem in single traversal.
+    input_list = input_list[:]
+
+    lo = 0                    # pointer for 0s
+    mid = 0                   # pointer for 1s
+    hi = len(input_list) - 1  # pointer for 2s
+
+    while mid <= hi:
+        if input_list[mid] == 0:
+            input_list[lo], input_list[mid] = input_list[mid], input_list[lo]
+            mid, lo = mid + 1, lo + 1
+        elif input_list[mid] == 2:
+            input_list[mid], input_list[hi] = input_list[hi], input_list[mid]
+            hi -= 1
+        elif input_list[mid] == 1:
+            mid += 1
+        else:
+            print('Error: List should only contain 0,1,2')
+            return None
+
+    return input_list
+
 
 def test_function(test_case: List[int]) -> None:
     sorted_array = sort_012(test_case)
@@ -27,6 +44,10 @@ def test_function(test_case: List[int]) -> None:
     else:
         print("Fail")
 
+
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
-test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2,
+               2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([])               # empty lists should work as expected
+test_function([3, 3, 1, 2, 0])  # prints an error
